87948135846145f09d450b742528f9f0bd166932
MarcRuizenti <146344001+MarcRuizenti@users.noreply.github.com>
2023-12-18 09:24:45 +0100

    movimiento e inicio de teleport

--- Assets/Scripts/MovimientoTriangulo.cs
+++ Assets/Scripts/MovimientoTriangulo.cs
@@ -5,17 +5,30 @@ using UnityEngine;
 public class MovimientoTriangulo : MonoBehaviour
 {
    
-    public float speed;
+    public float speedRotacion;
+    public float speedMovimento;
     public string inputHorizontal;
     public string inputVertical;
-   
+    Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
     void Update()
     {
         float horizontal = Input.GetAxis(inputHorizontal);
 
+        horizontal *= Time.deltaTime * speedRotacion;
+
         float vertical = Input.GetAxis(inputVertical);
 
-        transform.position += new Vector3(horizontal, vertical, 0) * Time.deltaTime * speed;
+        if (vertical > 0)
+        {
+            rb.AddForce(transform.up * vertical * speedMovimento * Time.deltaTime);
+        }
 
+        transform.eulerAngles += new Vector3(0, 0, horizontal);
     }
 }
